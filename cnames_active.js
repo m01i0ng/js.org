faff368f7ddbea68985fdb36d9b4b89d656c37cc
ManojGowda89 <mail@manojgowda.in>
2026-05-11 17:34:27 +0530

    fix

--- cnames_active.js
+++ cnames_active.js
@@ -105,7 +105,6 @@ var cnames_active = {
   "aditya": "aditya81070.github.io",
   "adnanbabakan": "adnanbabakan.github.io",
   "adon988": "adon988.github.io",
-  "toonkit": "manojgowda89.github.io/toonkit",
   "adv": "advjs.github.io",
   "advancedrpc": "advancedrpc.github.io",
   "aerogel": "noeldemartin.github.io/aerogel",
@@ -3397,6 +3396,7 @@ var cnames_active = {
   "toolost": "toolost-docs.netlify.app", // noCF
   "tools-kit": "blackb1rd-development.github.io/tools-kit",
   "toolzar": "toolzar.netlify.app",
+  "toonkit": "manojgowda89.github.io/toonkit",
   "topbot": "resign05.github.io/TobWebsite",
   "topgg": "topgg-node-sdk-docs.netlify.app",
   "tori": "boostcode.github.io/torijs",
